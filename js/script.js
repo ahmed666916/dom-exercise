8a8c0e137b5ff04d0327262893aa16a5a2b721e2
ahmed666916 <ahmed666916@gmail.com>
2024-01-24 21:59:41 +0500

    added button container

--- js/script.js
+++ js/script.js
@@ -80,3 +80,12 @@ buttonBaby.addEventListener('click', alertFunction);
 btn.addEventListener('click', function (e) {
     e.target.style.background = 'blue';
   });
+
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        alert(button.id);
+    })
+})
